0d82fec73af18797df1e5be977e99908f274c226
Victor Gila Barreto <gilabarreto@gmail.com>
2022-06-24 21:54:10 +0000

    Create a URL submission form, add a Get Route to show the Form, add post route to receive the form submission and implement a function that returns a string of 6 alphanumeric characters

--- express_server.js
+++ express_server.js
@@ -1,8 +1,23 @@
+// DESCRIPTION: Server to TinyApp Project
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080\
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({   extended: true }));
+
+/* 
+body-parser package deprecated. use express built-in version
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true})); 
+*/
+
+// returns a string of 6 random alphanumeric characters
+function generateRandomString() {
+  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,7 +45,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// add a POST Route to receive the Form Submission
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+
