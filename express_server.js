d82fde868682294272f0d4613cc648e8ab9c69a6
Victor Gila Barreto <gilabarreto@gmail.com>
2022-06-24 20:18:35 +0000

    Add second router and header

--- express_server.js
+++ express_server.js
@@ -30,8 +30,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-/* app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL:  
-};
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
-}); */
+});
