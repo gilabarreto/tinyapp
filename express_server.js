77dc678d86a5e99e939eddc56a3d33a4e46c32cc
Victor Gila Barreto <gilabarreto@gmail.com>
2022-06-25 22:28:23 +0000

    redirect shorturls

--- express_server.js
+++ express_server.js
@@ -1,43 +1,46 @@
-// DESCRIPTION: Server to TinyApp Project
+// DESCRIPTION: Server for TinyApp Project.
 
+// Constants and Requires
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080\
-
-app.set("view engine", "ejs");
-app.use(express.urlencoded({   extended: true }));
-
-/* 
-body-parser package deprecated. use express built-in version
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true})); 
-*/
-
-// returns a string of 6 random alphanumeric characters
-function generateRandomString() {
-  return Math.random().toString(16).slice(2,8);
-}
-
+const PORT = 8080; // default port 8080
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// EJS View Engine
+app.set("view engine", "ejs");
 
+// Express body-parser
+app.use(express.urlencoded({ extended: true })); // body-parser package deprecated, use express built-in version.
+
+// Function that returns a string of 6 random alphanumeric characters.
+function generateRandomString() {
+  return Math.random().toString(16).slice(2, 8);
+}
+
+// Server Connection
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+///////////////////
+// GET Requests //
+/////////////////
+
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+// Send database as a response
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.get("/urls", (req, res) => {
@@ -45,12 +48,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-// add a POST Route to receive the Form Submission
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
-
+// GET Route for URL Submission Form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -59,5 +57,27 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL
+  const longURL = urlDatabase[shortURL]
+  res.redirect(longURL);
+});
+
+/////////////////////
+// POST resquests //
+///////////////////
+
+// POST Route to receive the Form Submission.
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  let { longURL } = req.body;
+  if (!longURL.startsWith("http://") || !longURL.startsWith("https://")) {
+    longURL = `http://${longURL}`
+  };
+  urlDatabase[shortURL] = longURL;
+  res.redirect(`/urls/${shortURL}`)
+});
+
 
 
