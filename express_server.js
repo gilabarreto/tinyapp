258276acab51d1bb66fb085602b07d7126b753ed
Victor Gila Barreto <gilabarreto@gmail.com>
2022-06-28 17:10:33 +0000

    Add an endpoint to handle a POST to /login in your Express server.

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require("cookie-parser"); 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -15,12 +16,15 @@ app.set("view engine", "ejs");
 // Express body-parser
 app.use(express.urlencoded({ extended: true })); // body-parser package deprecated, use express built-in version.
 
+// Express cookie-parser
+app.use(cookieParser());
+
 // Function that returns a string of 6 random alphanumeric characters.
 function generateRandomString() {
   return Math.random().toString(16).slice(2, 8);
 }
 
-// Server Connection
+// Establish Server Connection
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -38,7 +42,7 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-// Send database as a response
+// Send Database as a Response
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -48,7 +52,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-// GET route for URL Submission Form
+// GET Route for URL Submission Form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -58,6 +62,7 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+// Redirect any Request to "/u/:shortURL" to its longURL.
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL
   const longURL = urlDatabase[shortURL]
@@ -68,7 +73,7 @@ app.get("/u/:shortURL", (req, res) => {
 // POST Resquests //
 ///////////////////
 
-// POST route to receive the Form Submission.
+// POST Route to Receive the Form Submission.
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   let { longURL } = req.body;
@@ -79,14 +84,14 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`)
 });
 
-// POST route that removes a URL resource.
+// POST Route that Removes a URL Resource.
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL]
   res.redirect(`/urls`)
 });
 
-// POST POST route that updates a URL resource.
+// POST Route that Updates a URL Resource.
 app.post("/urls/:id", (req, res) => {
   const shortURL = req.params.id;
   let { longURL } = req.body;
@@ -95,4 +100,11 @@ app.post("/urls/:id", (req, res) => {
   };
   urlDatabase[shortURL] = longURL;
   res.redirect(`/urls/${shortURL}`)
-})
+});
+
+// POST Route that Endpoint to Handles Login.
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect(`/urls`);
+});
